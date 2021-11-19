d1e0174de9ca049f68e8d1a857cebd644b200cea
Jesse Whiting <little.dude.jesse@gmail.com>
2021-11-19 10:37:21 -0700

    Camera and Input bug fix

--- Assets/Project/Scripts/Misc/SingletonComponent.cs
+++ Assets/Project/Scripts/Misc/SingletonComponent.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SingletonComponent<T> : MonoBehaviour where T : Component
 {
+
+    public static bool InstanceExists { get { return (instance != null); } }
     private static T instance;
     public static T Instance { get
         {
