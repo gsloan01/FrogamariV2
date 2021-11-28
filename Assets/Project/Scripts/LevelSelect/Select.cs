5e16f8042ec27ac0af1ba90c6cd5fb571ed88f65
CameronChildress <cameronchildress4@gmail.com>
2021-11-27 19:38:36 -0700

    Bouncing Kinda Works

--- Assets/Project/Scripts/LevelSelect/Select.cs
+++ Assets/Project/Scripts/LevelSelect/Select.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class Select : MonoBehaviour
 {
     public GameObject selectLevelUpCanvas;
+    public Transform selectLevelParent;
 
     void Start()
     {
@@ -30,7 +32,8 @@ public class Select : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             selectLevelUpCanvas.SetActive(true);
-
+            //selectLevelUpCanvas.transform.DOMove(new Vector3(selectLevelUpCanvas.transform.position.x, selectLevelUpCanvas.transform.position.y + 0.25f, selectLevelUpCanvas.transform.position.z), 2f).SetLoops(-1, LoopType.Yoyo);
+            selectLevelUpCanvas.transform.DOMove(new Vector3(selectLevelParent.transform.position.x, selectLevelParent.transform.position.y + 0.25f, selectLevelParent.transform.position.z - .5f), 2f).SetLoops(-1, LoopType.Yoyo);
             //Debug.Log("Entered");
         }
     }
