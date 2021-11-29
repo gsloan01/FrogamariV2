01668d8c3e5bc0070937193e24b86e143469a635
CameronChildress <cameronchildress4@gmail.com>
2021-11-28 17:59:16 -0700

    New level Select

--- Assets/Project/Scripts/LevelSelect/Select.cs
+++ Assets/Project/Scripts/LevelSelect/Select.cs
@@ -33,13 +33,16 @@ public class Select : MonoBehaviour
         {
             selectLevelUpCanvas.SetActive(true);
             //selectLevelUpCanvas.transform.DOMove(new Vector3(selectLevelUpCanvas.transform.position.x, selectLevelUpCanvas.transform.position.y + 0.25f, selectLevelUpCanvas.transform.position.z), 2f).SetLoops(-1, LoopType.Yoyo);
-            selectLevelUpCanvas.transform.DOMove(new Vector3(selectLevelParent.transform.position.x, selectLevelParent.transform.position.y + 0.25f, selectLevelParent.transform.position.z - .5f), 2f).SetLoops(-1, LoopType.Yoyo);
+            selectLevelUpCanvas.transform.DOMove(new Vector3(selectLevelUpCanvas.transform.position.x - 300, selectLevelUpCanvas.transform.position.y, selectLevelUpCanvas.transform.position.z), 2f);
             //Debug.Log("Entered");
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        selectLevelUpCanvas.SetActive(false);
+        selectLevelUpCanvas.transform.DOMove(new Vector3(selectLevelUpCanvas.transform.position.x + 300, selectLevelUpCanvas.transform.position.y, selectLevelUpCanvas.transform.position.z), 2f);
+
+        if (selectLevelUpCanvas.transform.position.x >= selectLevelUpCanvas.transform.position.x + 300)
+            selectLevelUpCanvas.SetActive(false);
     }
 }
