94556e0a984248aaf793812b26b62cfa798c2189
Jesse Whiting <little.dude.jesse@gmail.com>
2021-12-02 16:05:19 -0700

    LevelManager started

---
+++ Assets/Project/Scripts/Level/LevelManager.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LevelManager : SingletonComponent<LevelManager>
+{
+    [SerializeField] private float levelTime;
+    [SerializeField] private List<MassEvent> events;
+
+    private void Start()
+    {
+        MassManager.massUpdate += OnMassUpdate;
+    }
+
+
+    private void OnMassUpdate(float mass)
+    {
+        for (int i = 0; i < events.Count; i++)
+        {
+            if (events[i].playerMass < mass && !events[i].triggered)
+            {
+                events[i].triggered = true;
+                events[i].invokedEvents?.Invoke();
+            }
+        }
+    }
+}
+
+[Serializable]
+public class MassEvent
+{
+    public bool triggered;
+    public float playerMass;
+    public UnityEvent invokedEvents;
+}
